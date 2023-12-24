53db0351d855c9774c13640cdfd9e48b0eb32c05
Sebastian <115075552+SeboKnt@users.noreply.github.com>
2023-12-24 23:01:13 +0000

    add html

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -15,6 +15,8 @@ import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 import java.time.*;
 
+import java.util.*;
+
 /**
  * Azure Functions with HTTP Trigger.
  */
@@ -51,5 +53,22 @@ public class Function {
         final ExecutionContext context
     ) {
         context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
+    }
+
+    @FunctionName("Html")
+    public HttpResponseMessage runHtml(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET}, route = "html") HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+    
+        context.getLogger().info("Java HTTP trigger processed a request.");
+    
+        // Erstelle den HTML-Inhalt
+        String htmlContent = "<html><body><h1>Hallo von Azure Functions!</h1><p>Dies ist eine Beispiel-Webseite.</p></body></html>";
+    
+        // Erstelle die HTTP-Antwort mit dem HTML-Inhalt
+        return request.createResponseBuilder(HttpStatus.OK)
+                .header("Content-Type", "text/html")
+                .body(htmlContent)
+                .build();
     }
 }
