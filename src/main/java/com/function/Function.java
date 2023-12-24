e12e2dcaec20e9fd5c46b5fa21f4bf92e01f0b37
Sebastian <115075552+SeboKnt@users.noreply.github.com>
2023-12-24 23:54:15 +0000

    rm imports

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -1,20 +1,8 @@
 package com.function;
 
-import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpRequestMessage;
-import com.microsoft.azure.functions.HttpResponseMessage;
-import com.microsoft.azure.functions.HttpStatus;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
-import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
-
-import java.util.Optional;
-
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 import java.time.*;
-
 import java.util.*;
 
 public class Function {
