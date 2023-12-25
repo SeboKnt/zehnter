12e01101ac7eaed48351a8f64605aeb773de3867
Sebastian <115075552+SeboKnt@users.noreply.github.com>
2023-12-25 01:52:37 +0000

    every 5 min dm

--- src/main/java/com/function/Timer.java
+++ src/main/java/com/function/Timer.java
@@ -5,12 +5,12 @@ import com.microsoft.azure.functions.*;
 import java.time.*;
 
 public class Timer {
-
     @FunctionName("Timer")
-    public void timerHandler(
-        @TimerTrigger(name = "timerInfo", schedule = "0 */5 * * * *") String timerInfo,
-        final ExecutionContext context
-    ) {
-        context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
+    public void timerHandler(@TimerTrigger(name = "timer", schedule = "0 */5 * * * *") String timer) {
+
+        final String message = "HelloWord";
+
+        Telegram dm = new Telegram(message);
+        dm.sendMessage();
     }
 }
