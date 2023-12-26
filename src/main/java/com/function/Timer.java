64142ff9a28699e9093f15e78a10093fc397c47d
Sebastian <115075552+SeboKnt@users.noreply.github.com>
2023-12-26 14:10:51 +0000

    change timer

--- src/main/java/com/function/Timer.java
+++ src/main/java/com/function/Timer.java
@@ -6,9 +6,10 @@ import java.time.*;
 
 public class Timer {
     @FunctionName("Timer")
-    public void timerHandler(@TimerTrigger(name = "timer", schedule = "0 */5 * * * *") String timer) {
+    // On every 10th of the month at 9:00 AM
+    public void timerHandler(@TimerTrigger(name = "timer", schedule = "0 0 9 10 * *") String timer) {
 
-        final String message = "HelloWord";
+        final String message = "Stundenzettel für den Monat ist fällig!";
 
         Telegram dm = new Telegram(message);
         dm.sendMessage();
